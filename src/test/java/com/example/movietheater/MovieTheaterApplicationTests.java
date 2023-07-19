baad93362c1b068b30e1c6f2d5e3cafd392726da
khoa954 <87458205+khoa954@users.noreply.github.com>
2023-07-19 16:45:11 +0700

    add all file

---
+++ src/test/java/com/example/movietheater/MovieTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movietheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
